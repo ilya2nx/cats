b745f20d899b9b229b2dfad2da50378d302ab8f3
ilya2nx <mishanin_95@mail.ru>
2021-07-01 17:32:06 +0300

    first add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TileComponent } from './tile/tile.component';
+import { TableComponent } from './table/table.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TileComponent,
+    TableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
